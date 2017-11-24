d27691421a7f43ade0b2508e93dc3bf0130f7f65
CSharpFri <CSharpFri@CLASS-2-08>
2017-11-24 19:00:08 +0100

    Add project files.

---
+++ Laborator/Laborator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator
+{
+    partial class Laborator
+    {
+        public Pristroj LaboratorniPristroj1;
+        public Pristroj LaboratorniPristroj2;
+
+        public void SpustVyzkum()
+        {
+            Vedouci vedouci = new Vedouci("Tomas");               
+
+
+            LaboratorniPristroj1 = new  Mikroskop();
+            LaboratorniPristroj2 = new Odstredivka();
+
+            LaboratorniPristroj1.Nazev = "hn301";
+            LaboratorniPristroj2.Nazev = "ods56";
+
+
+            IOvladani ovladani = (IOvladani)LaboratorniPristroj1;
+            ovladani.Zapni();
+
+            LaboratorniPristroj1.VypisDetailniInfo();
+            LaboratorniPristroj2.VypisDetailniInfo();
+
+        }
+    }
+}
---
+++ Laborator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Laborator Laborator = new Laborator();
+            Laborator.SpustVyzkum();
+
+            Console.ReadLine();
+        }
+    }
+}
